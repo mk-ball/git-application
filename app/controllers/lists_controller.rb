f63f2c6c963712669e8ba1fc568d273ccee0d72b
EC2 Default User <ec2-user@ip-172-31-19-7.ap-northeast-1.compute.internal>
2023-07-27 06:50:15 +0000

    [finish]アプリケーションを完成させよう-2章

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,30 @@
+class ListsController < ApplicationController
+  def new
+    # Viewへ渡すインスタンス変数の定義
+    @list = List.new
+  end
+  
+  def create
+    # データをうけとるインスタンス
+    list = List.new(list_params)
+    # データをdbに保存するメソッド
+    list.save
+    # トップ画面へリダイレクト
+    redirect_to '/top'
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+  
+  private
+  # ストロングパラメータ
+  def list_params
+    params.require(:list).permit(:title, :body)
+  end
+end
